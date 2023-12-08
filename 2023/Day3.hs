9d3b35dc79f885d395ccf7ec220088fa9996d87f
Isaac Lopez <isaac.lopez@upr.edu>
2023-12-08 19:30:53 -0400

    finished day 3

--- 2023/Day3.hs
+++ 2023/Day3.hs
@@ -1,86 +1,75 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Day3 where
 
 import Data.Char
-import Prelude hiding (head, init, last, tail)
-
-class Deque q where
-    empty :: q a
-    isEmpty :: q a -> Bool
-    cons :: a -> q a -> q a
-    head :: q a -> a
-    tail :: q a -> q a
-    snoc :: q a -> a -> q a
-    last :: q a -> a
-    init :: q a -> q a
-
-data DList a = DL Int [a] Int [a] deriving (Show)
-
-checkf :: Int -> [a] -> Int -> [a] -> DList a
-checkf lenf f lenr r
-    | lenf > c * lenr + 1 =
-        let i = (lenf + lenr) `div` 2
-            j = lenf + lenr - i
-            f' = take i f
-            r' = r ++ reverse (drop i f)
-         in DL i f' j r'
-    | lenr > c * lenf + 1 =
-        let j = (lenf + lenr) `div` 2
-            i = lenf + lenr - j
-            r' = take j r
-            f' = f ++ reverse (drop j r)
-         in DL i f' j r'
-    | otherwise = DL lenf f lenr r
-  where
-    c = 3
-
-instance Deque DList where
-    empty = DL 0 [] 0 []
-    isEmpty (DL lenf _ lenr _) = lenf + lenr == 0
-    cons x (DL lenf f lenr r) = checkf (lenf + 1) (x : f) lenr r
-    head (DL _ [] _ _) = error "empty deque"
-    head (DL _ (x : _) _ _) = x
-    tail (DL _ [] _ _) = error "empty deque"
-    tail (DL lenf (_ : f) lenr r) = checkf (lenf - 1) f lenr r
-    snoc (DL lenf f lenr r) x = checkf lenf f (lenr + 1) (x : r)
-    last (DL _ _ _ []) = error "empty deque"
-    last (DL _ _ _ (x : _)) = x
-    init (DL _ _ _ []) = error "empty deque"
-    init (DL lenf f lenr (x : r)) = checkf lenf f (lenr - 1) r
-
-fromList :: [a] -> DList a
-fromList = foldr cons empty
+import Data.Either (partitionEithers)
+import Data.Maybe (isJust, maybeToList)
+import System.Environment (getArgs)
 
 -- representation of values
 data EnginePart = EnginePart
     { part :: String
-    , idx :: [Int]
+    , ln :: Int
+    , scol :: Int
+    , ecol :: Int
     }
     deriving (Show)
 
-type Schematic = [EnginePart]
+data Schematic = Schematic
+    { eps :: [EnginePart]
+    , sym :: [((Int, Int), Char)]
+    }
+    deriving (Show)
 
 -- mk a tuple of number/symbol w indexes
-helper1 :: String -> [EnginePart]
-helper1 xs = insideH xs 0
+helper1 :: String -> [Either ((Int, Int), Char) EnginePart]
+helper1 xs = insideH xs 0 0
   where
-    insideH [] _ = []
-    insideH (x : xs) y
-        | x == '.' = insideH xs (y + 1)
-        | isDigit x =
-            let (part, left) = (x : takeWhile isDigit xs, dropWhile isDigit xs)
-                idx = [y .. y + length part - 1]
-             in EnginePart{..} : insideH left (y + length part)
-        | otherwise = EnginePart [x] [y] : insideH xs (y + 1)
+    insideH [] _ _ = []
+    insideH (c : cs) x y
+        | c == '\n' = insideH cs 0 (y + 1)
+        | c == '.' = insideH cs (x + 1) y
+        | isDigit c =
+            let (part, left) = (c : takeWhile isDigit cs, dropWhile isDigit cs)
+                ln = y
+                scol = x
+                ecol = x + length part - 1
+             in Right EnginePart{..} : insideH left (x + length part) y
+        | otherwise = Left ((y, x), c) : insideH cs (x + 1) y
+
+near :: EnginePart -> [(Int, Int)]
+near EnginePart{..} = [(l, col) | l <- [pred ln .. succ ln], col <- [pred scol .. succ ecol]]
+
+eligible :: Schematic -> [Int]
+eligible Schematic{..} = [read $ part e | e <- eps, nearSym e]
+  where
+    nearSym pp = any (isJust . flip lookup sym) $ near pp
+
+part1 :: IO ()
+part1 = getArgs >>= readFile . head >>= print . sum . eligible . helper2 . helper1
 
 -- mk a list of schematics by lines
-helper2 :: String -> DList Schematic
-helper2 = fromList . map helper1 . lines
-
--- check if index list are in bounds
-check :: [Int] -> [Int] -> Bool
-check xs ys = (not . null) [x | x <- xs, y <- ys, let v = x - y in v >= -1 && v <= 1]
+helper2 :: [Either ((Int, Int), Char) EnginePart] -> Schematic
+helper2 (partitionEithers -> schem) = Schematic (snd schem) (fst schem)
 
 example1 :: String
 example1 = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+
+nearGears :: Schematic -> [((Int, Int), EnginePart)]
+nearGears Schematic{..} = [(rc, p) | p <- eps, rc <- near p, '*' <- maybeToList $ lookup rc sym]
+
+filterGears :: [((Int, Int), EnginePart)] -> [((Int, Int), EnginePart)]
+filterGears xs = [(r, p) | (r, p) <- xs, length (filter ((== r) . fst) xs) == 2]
+
+gearRatio :: [((Int, Int), EnginePart)] -> [Int]
+gearRatio [] = []
+gearRatio (x : xs) =
+    let rc = fst x
+     in case lookup rc xs of
+            Nothing -> gearRatio xs
+            Just ep -> read (part ep) * read (part $ snd x) : gearRatio xs
+
+part2 :: IO ()
+part2 = getArgs >>= readFile . head >>= print . sum . gearRatio . filterGears . nearGears . helper2 . helper1
