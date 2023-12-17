7cc11904c0f2e3db9b7fe72161fc0fc2475dbb56
Isaac Lopez <isaac.lopez@upr.edu>
2023-12-16 22:34:43 -0400

    finished part 1 day 8

--- 2023/Day5.hs
+++ 2023/Day5.hs
@@ -1,15 +1,16 @@
 module Day5 where
 
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf, sortOn)
 import Data.Maybe (fromMaybe)
 
 mkRng :: Int -> Int -> [Int]
 mkRng i n = [i .. (i + n - 1)]
 
 mkMaps :: Int -> Int -> Int -> [(Int, Int)]
-mkMaps s d l = zip (mkRng s l) (mkRng d l)
+mkMaps s d l = sortOn fst $ zip (mkRng s l) (mkRng d l)
 
 find :: Int -> [(Int, Int)] -> Int
 find n m = fromMaybe n (lookup n m)
 
+example :: String
 example = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
--- 2023/Day8.hs
+++ 2023/Day8.hs
@@ -1,16 +1,44 @@
 module Day8 where
 
+import Data.Char (isAlphaNum)
 import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
 
 move ::
     String ->
-    (String, (String, String)) ->
+    (String, String) ->
     [(String, (String, String))] ->
+    Int ->
     Int
-move [] _ _ = undefined
-move ('L' : m) a@(h, (l, r)) xs =
-    if l == "ZZZ"
-        then 1
-        else
-            let nm = fromMaybe (l, r) (lookup h xs)
-             in 1 + move m nm (tail xs ++ [a])
+move (x : m) (l, r) xs n
+    | x == 'L' && l == "ZZZ" = n
+    | x == 'R' && r == "ZZZ" = n
+    | x == 'R' =
+        let nm = lookup r xs
+         in case nm of
+                Nothing -> error "looked for something that doesn't exist!"
+                Just x -> move (m ++ "R") x xs (n + 1)
+    | otherwise =
+        let nm = lookup l xs
+         in case nm of
+                Nothing -> error "looked for something that doesn't exist!"
+                Just x -> move (m ++ "L") x xs (n + 1)
+
+parse :: String -> (String, (String, String))
+parse xs =
+    let v = splitAt 3 $ filter isAlphaNum $ drop 6 xs
+     in (take 3 xs, v)
+
+example1 :: String
+example1 = "RL\n\nAAA = (BBB, CCC)\nBBB = (DDD, EEE)\nCCC = (ZZZ, GGG)\nDDD = (DDD, DDD)\nEEE = (EEE, EEE)\nGGG = (GGG, GGG)\nZZZ = (ZZZ, ZZZ)"
+
+example2 :: String
+example2 = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)"
+
+part1 :: IO ()
+part1 = do
+    args <- getArgs
+    c <- lines <$> readFile (head args)
+    let x = concat $ take 1 c
+        v = map parse (drop 2 c)
+    print $ move x (fromMaybe (snd $ head v) (lookup "AAA" v)) v 1
